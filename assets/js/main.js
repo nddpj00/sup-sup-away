c161adba059467020bb87d36dbc8637b765b0cea
nddpj00 <danieljones625@btinternet.com>
2023-01-09 22:50:55 +0000

    tidy code removing whitespace and unnecessary semi-colons

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@ const finalUserDestination = [];
 const questions = [{
   id:1,
   q: "Where would you like to paddle?",
-  a: ["Sea", "Lake", "River"],
+  a: ["Sea", "Lake", "River"]
   },
 
   {id:2,
@@ -37,7 +37,7 @@ let selected ="";
 
 //events for start and restart buttons
 startButton.addEventListener('click', initQuestionnaire);
-restartButton.addEventListener('click', function(){location.reload()})
+restartButton.addEventListener('click', function(){location.reload();});
 
 // hides start button and initiates runQuestionnaire function
 function initQuestionnaire(){
@@ -65,48 +65,46 @@ function iterateQuestions(id){
     displayButton3.classList.add('hide');
   }else{
     displayButton3.innerHTML = questions[id].a[2];
-  };
+  }
 
-  displayButton1.addEventListener("click", () => {
+  displayButton1.addEventListener("click", function(){
     displayButton1.style.backgroundColor = "green";
     displayButton2.style.backgroundColor = "hsl(27, 88%, 66%)";
     displayButton3.style.backgroundColor = "hsl(27, 88%, 66%)";
     selected = displayButton1.innerText;
   });
 
-  displayButton2.addEventListener("click", () => {
+  displayButton2.addEventListener("click", function(){
     displayButton1.style.backgroundColor = "hsl(27, 88%, 66%)";
     displayButton2.style.backgroundColor = "green";
     displayButton3.style.backgroundColor = "hsl(27, 88%, 66%)";
     selected = displayButton2.innerText;
   });
 
-  displayButton3.addEventListener("click", () => {
+  displayButton3.addEventListener("click", function(){
     displayButton1.style.backgroundColor = "hsl(27, 88%, 66%)";
     displayButton2.style.backgroundColor = "hsl(27, 88%, 66%)";
     displayButton3.style.backgroundColor = "green";
     selected = displayButton3.innerText;
-  });  
-  
+  });
 }
 
-// loop for questions and takes user selection to create new object 
+//loop for questions and takes user selection to create new object
 let id = 0;
-nextButton.addEventListener("click", () => {
-id++;  
-createUserDestObj(selected,id);  
+nextButton.addEventListener("click", function(){
+id++;
+createUserDestObj(selected,id);
 
- //checks for selection and if none made alert triggered
+//checks for selection and if none made alert triggered
 if(displayButton1.style.backgroundColor !== 'green' &&
     displayButton2.style.backgroundColor !== 'green' &&
     displayButton3.style.backgroundColor !== 'green'){
-    alert('Oops, dont forget to make a selection before clicking next.'); id--}
-    
-else if(id <= questions.length -1){
+    alert('Oops, dont forget to make a selection before clicking next.');
+  }else if(id <= questions.length -1){
   console.log(id, selected);
   iterateQuestions(id);
-    
-//returns background colour to orange    
+
+//returns background colour to orange
     displayButton1.style.backgroundColor = "hsl(27, 88%, 66%)";
     displayButton2.style.backgroundColor = "hsl(27, 88%, 66%)";
     displayButton3.style.backgroundColor = "hsl(27, 88%, 66%)";
@@ -123,14 +121,14 @@ function finalDestination() {
   const finalUserWebsite = [];
 //contains the indexs of the locations that match the users criteria
   let matchedIndicesArray = compareLocation();
-  console.log(matchedIndicesArray)
+  console.log(matchedIndicesArray);
 
 // creates an array of the location array name values
-  let destNames = locationArray.map(object => object.name);
-  let destMarkers = locationArray.map(object1 => object1.marker);
-  let destWeb = locationArray.map(object2 => object2.website);
+  let destNames = locationArray.map((object) => object.name);
+  let destMarkers = locationArray.map((object1) => object1.marker);
+  let destWeb = locationArray.map((object2) => object2.website);
 
-/*loops the machedIndicesArray pushing each selection to new arrays 
+/*loops the machedIndicesArray pushing each selection to new arrays
 'finalUserDestination''finalUserMarkers'& 'finalUserWebsite' that contain the destination
 names, co-ordinates and urls respectively*/
   for(i=0; i < matchedIndicesArray.length ; i++){
@@ -160,16 +158,16 @@ names, co-ordinates and urls respectively*/
     }else{
     map.setZoom(8);
     map.setCenter(marker.getPosition());
-  
+
   infowindow.open({
     anchor: marker,
     map,
-  })};});
-};
-
+  });
+  }
+});
+}
 document.getElementById('result-text').innerHTML += finalUserDestination.join('<br/>');
-  
-};
+}
 
 // creates a new object from users selections
 let resultObject = new Object();
@@ -183,7 +181,7 @@ return resultObject;
 }
 console.log(resultObject);
 
-/* stringifies user selections (resultObject) and creates duplicate, 
+/*stringifies user selections (resultObject) and creates duplicate,
 removing name and website keys. Then compares duplicate to stringified locationArry*/
 
 function compareLocation(){
@@ -201,13 +199,13 @@ for (let i = 0; i < locationArray.length; i++) {
   // convert and compare
   if (userDataString === JSON.stringify(duplicateObject)) {
   matchedIndices.push(i);
-    }
-};
+  }
+}
 return matchedIndices;
-};
+}
 
 const finalUserMarkers =[];
-console.log(finalUserMarkers)
+console.log(finalUserMarkers);
 
 // Map code
 let map;
